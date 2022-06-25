8b0dd30800dedc1ebbb2dc475c7f14c741687c3e
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2022-06-25 10:41:46 +0430

    option delete is complete

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ var buttonadd = document.querySelector("#add").addEventListener("click", functio
     addperson()
 })
 
+var table = document.querySelector("#table");
+
 function addperson() {
-    var table = document.querySelector("#table")
     var fname = document.querySelector("#firstName").value;
     var lname = document.querySelector("#lastName").value;
     var person = document.createElement("tr");
@@ -12,7 +13,10 @@ function addperson() {
     var operations = document.createElement("td");
     var delettag = document.createElement("a");
     delettag.textContent = "Delete"
-    delettag.href= "#";
+    delettag.href = "#";
+    delettag.addEventListener("click", function () {
+        deleteperson(person)
+    })
     fnamecell.textContent = fname;
     lnamecell.textContent = lname;
     person.appendChild(fnamecell);
@@ -22,4 +26,8 @@ function addperson() {
     table.appendChild(person);
     console.log(table);
 
+}
+
+function deleteperson(per) {
+    table.removeChild(per);
 }
